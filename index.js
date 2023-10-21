b5720f1b66b989119a04f33addb83c8b69cf6c57
= <=>
2023-10-21 13:14:53 +0530

    db

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 const server = express();
 const port = process.env.PORT;
 import loadRouter from "./routes/loads.js";
-import connectDb from "./database/db.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.js";
@@ -12,6 +11,18 @@ import bidsController from "./routes/bid.js";
 import adminRouter from "./routes/admin.js";
 import path from "path";
 import { fileURLToPath } from "url";
+
+import mongoose from "mongoose";
+async function connectDb() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://arorasahil074:kNd71q8R9soM82Mm@cluster0.7dyklld.mongodb.net/"
+    );
+    console.log("Connected to database");
+  } catch (err) {
+    console.log(`Error in connecting with database ${err}`);
+  }
+}
 
 connectDb();
 server.use(
