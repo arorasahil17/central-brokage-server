563b9b0ad119c7807b2f6a16ffc261c2d6c94fdd
= <=>
2023-10-21 12:51:23 +0530

    db

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ async function connectDb() {
     await mongoose.connect(
       "mongodb+srv://arorasahil074:kNd71q8R9soM82Mm@cluster0.7dyklld.mongodb.net/"
     );
-    console.log("Database Connected");
+    console.log("Connected to database");
   } catch (err) {
     console.log(`Error in connecting with database ${err}`);
   }
